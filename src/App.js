c1d6b364b53c04d9a8703160f1643bc1998bc3e5
Sid <sidoniomiguelsilva567@gmail.com>
2024-04-05 10:41:15 +0100

    Structural commit 2

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Home, About, Education, Experience, Hobbies, Navbar, Skills} from './components';
 
 function App() {
   return (
     <Router>
       <>
         <Route exact path="/" component={Home} />
+        <Switch>
+          <Route path='/about' component={About} />          
+          <Route path='/skills' component={Skills} />
+          <Route path='/education' component={Education} />
+          <Route path='/experience' component={Experience} />
+          <Route path='/hobbies' component={Hobbies} />
+        </Switch>
       </>
     </Router>
   );
