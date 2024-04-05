9cebde58e8ee1e6a90616b77b15ed79e3ac4db77
Sid <sidoniomiguelsilva567@gmail.com>
2024-04-05 10:36:23 +0100

    Structural commit

---
+++ src/components/Hobbies.js
@@ -0,0 +1,25 @@
+import {React, Component} from 'react';
+
+export default class Hobbies extends Component{
+    render(){
+        return(
+            <section>
+                <NavBar />
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col s12 m4 l3'>
+                            <Profile />
+                        </div>
+                        <div className='col s12 m8 l9'>
+                            <About />
+                            <Skills />
+                            <Experience />
+                            <Education />
+                            <Hobbies />
+                        </div>
+                    </div>
+                </div>
+            </section>
+        )
+    }
+}
