3e9f3655cdfe8256e169906cde5241666ed62894
Sid <sidoniomiguelsilva567@gmail.com>
2024-04-17 23:27:11 +0100

    Updated About Me section and included a pop up to contact me

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,41 @@
 import {React, Component} from 'react';
-
+import Popup from 'reactjs-popup';
+import ContactForm from './ContactForm'
 
 export default class About extends Component{
     render(){
         return(
             <div>
-                
+                <div className="card">
+                    <div className='card-content'>
+                        <h6 className='mt bottom'>About me</h6>
+                        <p className='grey-text'>Motivated young professional in the tech industry, seeking a driven and collaborative team, fostering a culture of high-quality project delivery.</p>
+                    </div>
+                    <div className='card-action'>
+                        <h6>Personal Information</h6>
+                        <div className='row mt'>
+                            <div className='col s12 m6 l6 xl6'>
+                                <p>
+                                    <strong>Location:</strong> London, UK
+                                </p>
+                                <p>
+                                    <strong>Contact:</strong> 
+                                    <Popup trigger={<a> Complete form here.</a>}>
+                                        <ContactForm />
+                                    </Popup>
+                                </p>
+                            </div>
+                            <div className='col s12 m6 l6 xl6'>
+                                <p>
+                                    <strong>Native Language:</strong> Portuguese
+                                </p>
+                                <p>
+                                    <strong>Second Language:</strong> English (Fluent)
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
--- src/components/Home.js
+++ src/components/Home.js
@@ -14,10 +14,10 @@ export default class Home extends Component{
                 <Navbar />
                 <div className='container'>
                     <div className='row'>
-                        <div className='col s12 m5 l4'>
+                        <div className='col s10 m4 l4'>
                             <Profile />
                         </div>
-                        <div className='col s12 m8 l9'>
+                        <div className='col s12 m8 l8'>
                             <About />
                             <Skills />
                             <Experience />
