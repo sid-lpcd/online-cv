a428cbfca79f08666f59b0b97e589e82a8b01f16
Sid <sidoniomiguelsilva567@gmail.com>
2024-10-27 12:25:14 +0000

    Update CV

---
+++ src/components/Banner.js
@@ -0,0 +1,23 @@
+export default function Banner(){
+    return(
+        <div className="banner-main row">
+            <div className="col s6 m4 l4 xl4">
+                <div className="translation-text">
+                    <h1>Welcome</h1>
+                    <h1>Bem-vindo</h1>
+                    <h1>...</h1>
+                </div>
+                <h1>I'm Sid Silva</h1>
+                <div>
+                    <h3>Volleyball Enthusiaste</h3>
+                    <h3>Front-End Developer</h3>
+                    <h3>Solo Traveler</h3>
+                    <h3>Educational Volunteer</h3>
+                </div>
+            </div>
+            <div className="col s6 m8 l8 xl8">
+
+            </div>
+        </div>
+    )
+}
